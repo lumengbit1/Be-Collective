21dca05ef4c3684ecc122f6ade8f8e2732812b5e
Administrator <lumengbit1@gmail.com>
2018-10-06 00:15:21 +1000

    v0.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ITree from './Tree';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+            <ITree/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
